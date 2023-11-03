a8e05305d2acd95c4cae2cebfbc5d5603eb3b2bd
Siddharth <goraisiddharth@gmail.com>
2023-11-03 16:44:07 +0530

    3nd Nov 16:43

--- app/src/main/java/com/ism/deliverism/MainActivity.kt
+++ app/src/main/java/com/ism/deliverism/MainActivity.kt
@@ -2,25 +2,51 @@ package com.ism.deliverism
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.ism.deliverism.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    lateinit var toolbar: Toolbar
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var navView: NavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
 
+        //nav drawer
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.nav_view)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, 0, 0
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+//                navView.setNavigationItemSelectedListener(this)
+        //        toggle.setDrawerIndicatorEnabled(false)
+        toolbar.setNavigationIcon(R.drawable.hamburger)
+
+
+            // bottom nav view
         navController = Navigation.findNavController(this, R.id.activity_main_nav_host_fragment)
         setupWithNavController(binding.bottomNavigationView, navController)
 
 
     }
+
 
 }
