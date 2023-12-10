5adb90cd9c9f1a73402a72f8655c0c61a9b53ea1
Siddharth <goraisiddharth@gmail.com>
2023-12-10 08:33:26 +0530

    10th Dec 8:33

--- app/src/main/java/com/ism/deliverism/ProfileFragment.kt
+++ app/src/main/java/com/ism/deliverism/ProfileFragment.kt
@@ -6,14 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
--- app/src/main/java/com/ism/deliverism/cartActivity.kt
+++ app/src/main/java/com/ism/deliverism/cartActivity.kt
@@ -1,11 +1,29 @@
 package com.ism.deliverism
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.ism.deliverism.databinding.ActivityCartBinding
+
 
 class cartActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCartBinding
+    lateinit var cartToolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cart)
+        binding = ActivityCartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        cartToolbar = binding.toolbarCart
+        cartToolbar.setTitle("Cart")
+        setSupportActionBar(cartToolbar)
+
+        cartToolbar.setNavigationIcon(R.drawable.baseline_chevron_left_24)
+        cartToolbar.setNavigationOnClickListener {
+          onBackPressed()
+        }
+
+
     }
 }
